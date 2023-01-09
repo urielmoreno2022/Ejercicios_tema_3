699b6d4ef29f1299efc66aac329001585197ddbe
Uriel <Uriel@LAPTOP-B79C0EOJ>
2023-01-09 09:56:07 -0500

    Repositorio - Ejercicios tema 3 - Suma

---
+++ src/ejercicios_tema_3_suma/Ejercicios_tema_3_suma.java
@@ -0,0 +1,19 @@
+
+package ejercicios_tema_3_suma;
+
+/**
+ *
+ * @author Uriel
+ */
+public class Ejercicios_tema_3_suma {
+
+    private static int suma(int a, int b, int c) {
+        int sumar = a + b + c;
+        return(sumar);
+    }
+    public static void main(String[] args) {
+        int a=20, b=15,c=25;
+        System.out.println (suma(a,b,c));
+    }
+    
+}
